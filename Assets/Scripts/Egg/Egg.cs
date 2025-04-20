404ed3d278977d38bb5f8a6d9a1fe0e7417ace00
Polalf <103451330+Polalf@users.noreply.github.com>
2025-04-20 15:32:02 -0400

    Sprites

--- Assets/Scripts/Egg/Egg.cs
+++ Assets/Scripts/Egg/Egg.cs
@@ -1,8 +1,22 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Egg : MonoBehaviour
 {
     [SerializeField] AudioClip pickedAudio;
+
+    [SerializeField] private List<Sprite> sprites = new List<Sprite>();
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        if (!spriteRenderer)
+        {
+            spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        }
+
+        spriteRenderer.sprite = sprites[Random.Range(0,sprites.Count)];
+    }
     public void Collected(Player _player)
     {
         Spawner.instance.EggsCount--;
